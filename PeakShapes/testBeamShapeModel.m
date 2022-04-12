7ce7e1167c4e78ba0973e857b41eb0c92261e6b9
Noah McLean <noahmc@ku.edu>
2022-04-12 12:12:02 -0500

    test result: nonunique solution

--- PeakShapes/testBeamShapeModel.m
+++ PeakShapes/testBeamShapeModel.m
@@ -3,16 +3,42 @@
 
 massSpec = setupMassSpec("PhoenixKansas_1e12");
 
-magnetMasses = 204.65:0.001:205.35;
-modelMassRange = [204.75 205.25];
+magnetMasses = 204.6:0.001:205.4;
+modelMassRange = [204.7 205.3];
 
 [G, modelMasses] = assembleG(magnetMasses, massSpec, modelMassRange);
 
 nModelMasses = length(modelMasses);
+deltaModelMass = modelMasses(2) - modelMasses(1);
+
 beam = zeros(size(modelMasses));
-beamCenter = 
+if ~mod(nModelMasses,2) % if even
+    beamCenterIndex = [nModelMasses/2 nModelMasses/2+1];
+else
+    beamCenterIndex = ceil(nModelMasses/2);
+end % if
 
-measIntensity = G*beam;
-plot(magnetMasses, measIntensity)
+beamWidthAMU = 205/massSpec.effectiveRadiusMagnetMM*0.30;
+beamDensity = 1e5; 
+halfBeamWidthIndices = round(beamWidthAMU/deltaModelMass/2);
 
+beamStartIndex = beamCenterIndex(1) - halfBeamWidthIndices;
+beamStopIndex  = beamCenterIndex(end) + halfBeamWidthIndices;
+beam(beamStartIndex:beamStopIndex) = beamDensity;
+totalBeam = (modelMasses(beamStopIndex)-modelMasses(beamStartIndex))*beamDensity;
+
+trueCounts = G*beam * 0.2; % 0.2 seconds of integration
+measCounts = poissrnd(trueCounts);
+
+plot(magnetMasses, trueCounts, magnetMasses, measCounts)
 xlim([magnetMasses(1) magnetMasses(end)])
+
+Wdata = diag(1./max(measCounts,1));
+beamSolution0 = G\trueCounts;
+beamSolution1 = (G'*Wdata*G)\(G'*Wdata*measCounts);
+beamSolution2 = lsqnonneg(G,measCounts);
+
+meas1 = G*beamSolution1;
+meas2 = G*beamSolution2;
+
+plot(modelMasses, beamSolution0, modelMasses, beamSolution2)
