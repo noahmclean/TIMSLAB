55d59dfbddd93d9071d2eb59b67e6b8757c388ca
Noah McLean <noahmc@ku.edu>
2022-04-13 13:56:09 -0500

    more beam shape testing

--- PeakShapes/testBeamShapeModel.m
+++ PeakShapes/testBeamShapeModel.m
@@ -19,7 +19,7 @@ else
 end % if
 
 beamWidthAMU = 205/massSpec.effectiveRadiusMagnetMM*0.30;
-beamDensity = 1e5; 
+beamDensity = 1e4; 
 halfBeamWidthIndices = round(beamWidthAMU/deltaModelMass/2);
 
 beamStartIndex = beamCenterIndex(1) - halfBeamWidthIndices;
@@ -27,6 +27,7 @@ beamStopIndex  = beamCenterIndex(end) + halfBeamWidthIndices;
 beam(beamStartIndex:beamStopIndex) = beamDensity;
 totalBeam = (modelMasses(beamStopIndex)-modelMasses(beamStartIndex))*beamDensity;
 
+trueCPS = G*beam;
 trueCounts = G*beam * 0.2; % 0.2 seconds of integration
 measCounts = poissrnd(trueCounts);
 
@@ -36,7 +37,7 @@ xlim([magnetMasses(1) magnetMasses(end)])
 Wdata = diag(1./max(measCounts,1));
 beamSolution0 = G\trueCounts;
 beamSolution1 = (G'*Wdata*G)\(G'*Wdata*measCounts);
-beamSolution2 = lsqnonneg(G,measCounts);
+beamSolution2 = lsqnonneg(chol(Wdata)*G,chol(Wdata)*measCounts);
 
 meas1 = G*beamSolution1;
 meas2 = G*beamSolution2;
