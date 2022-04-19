f91a03d15c4c5d11df4c79784637258894483e0d
Noah McLean <noahmc@ku.edu>
2022-04-19 08:42:56 -0500

    the problem was a wrong collectorWidth!

--- PeakShapes/beamShapeAsSpline.m
+++ PeakShapes/beamShapeAsSpline.m
@@ -26,20 +26,23 @@ theoreticalBeamWidthMM = 0.35; % mm
 %% now try again, with limited number of spline segments, no penalty
 
 beamWidthAMU = massAtCenterAMU / massSpec.effectiveRadiusMagnetMM * theoreticalBeamWidthMM;
-collectorWidthAMU = massAtCenterAMU / massSpec.effectiveRadiusMagnetMM * massSpec.collectorWidthMM;
+%collectorWidthAMU = massAtCenterAMU / massSpec.effectiveRadiusMagnetMM * massSpec.collectorWidthMM;
+collectorWidthAMU = 0.36;
 beamWindow = beamWidthAMU*2;
 collectorLimits = magnetMasses + [-collectorWidthAMU, collectorWidthAMU]/2;
 
 deltaMagnetMass = magnetMasses(2)-magnetMasses(1);
 
-beamKnots = floor(beamWindow/(deltaMagnetMass)) - 3; % 3 xtra knots for cubic spline
-nInterp = 500;
+bdeg = 3; % order of spline (= order of polynomial pieces)
+pord = 2; % order of differences 
+beamKnots = 2*floor(beamWindow/(deltaMagnetMass)) - bdeg; % 3 xtra knots for cubic spline
+nInterp = 500; % number of interpolated segments
 
 xl = massAtCenterAMU - beamWindow/2;
 xr = massAtCenterAMU + beamWindow/2;
 
 beamMassInterp = linspace(xl, xr, nInterp);
-B = bbase(beamMassInterp, xl, xr, beamKnots, 3);
+B = bbase(beamMassInterp, xl, xr, beamKnots, bdeg);
 deltabeamMassInterp = beamMassInterp(2)-beamMassInterp(1);
 
 % discrete convolution setup with design matrix G
@@ -59,47 +62,152 @@ for iMass = 1:nMagnetMasses % a row for each manget mass
 
 end
 
-%% give this a try?
-testBeam = [0 0 0 0 0 0 0 0 0 1 4 10 9 8 7 6 5 4 3 2 1 0 0 0 0 0]' * 5*10^5;
+% %% give this a try?
+% testBeam = [0 0 0 0 0 0 0 0 0 1 4 10 9 8 7 6 5 4 3 2 1 0 0 0 0 0]' * 5*10^4;
+% GB = G*B;
+% truePeakIntensity = G*B*testBeam;
+% 
+% figure('Position', [1 1 1200 1000], 'Units', 'pixels')
+% 
+% subplot(2,2,1)
+% plot(magnetMasses, truePeakIntensity, 'LineWidth', 2)
+% subplot(2,2,2)
+% hold on
+% plot(beamMassInterp, B*testBeam, 'LineWidth', 2)
+% 
+% beam1 = (G*B)\truePeakIntensity;
+% plot(beamMassInterp, B*beam1, ':r', 'LineWidth', 2)
+% 
+% 
+% % ok!  add noise.
+% integrationTime = 0.2; % seconds
+% measPeakIntensity = poissrnd(truePeakIntensity*integrationTime);
+% Wdata = diag(1./max(measPeakIntensity,1));
+% beam2 = (GB'*Wdata*GB)\(GB'*Wdata*measPeakIntensity);
+% beam3 = GB\measPeakIntensity;
+% subplot(2,2,3)
+% plot(magnetMasses, measPeakIntensity, 'LineWidth', 2);
+% subplot(2,2,4)
+% hold on
+% plot(beamMassInterp, B*beam3*5, '-b', 'LineWidth', 2)
+% plot(beamMassInterp, B*beam2*5, ':r', 'LineWidth', 2)
+% 
+% % results not the best, but not horrible!
+% 
+% % now let's try with some smoothing
+% 
+% lambda = 1e-10;
+% D = diff(eye(beamKnots+bdeg), pord); % 2nd order smoothing, cubic spline;
+% 
+% Gaugmented = [GB; sqrt(lambda)*D];
+% measAugmented = [measPeakIntensity; zeros(beamKnots+bdeg-pord,1)];
+% wtsAugmented = blkdiag(Wdata, eye(beamKnots+bdeg-pord));
+% beamSmooth = (Gaugmented'*wtsAugmented*Gaugmented)\(Gaugmented'*wtsAugmented*measAugmented);
+% plot(beamMassInterp, B*beamSmooth*5, '-g', 'LineWidth', 2)
+% 
+% % smoother!  need to replace magic numbers (24) above.
+% 
+
+%% try on real data
+
+filename = "DVCC18-9 z9 Pb-570-PKC-205Pb-PM-S2B7C1.txt";
+%filename = "NBS987 StaticAxH1H2 Bead1Run1-393-PKC-86Sr-Ax-S1B8C1.txt";
+data = parsePeakCenterDataFile(filename);
+
+magnetMasses = data.meas(:,1);
+measPeakIntensity = data.meas(:,2);
+massAtCenterAMU = data.peakCenterMass;
+
+collectorLimits = magnetMasses + [-collectorWidthAMU, collectorWidthAMU]/2;
+deltaMagnetMass = magnetMasses(2)-magnetMasses(1);
+
+bdeg = 3; % order of spline (= order of polynomial pieces)
+pord = 2; % order of differences 
+nInterp = 5000; % number of interpolated segments
+
+xl = massAtCenterAMU - beamWindow/2;
+xr = massAtCenterAMU + beamWindow/2;
+
+beamMassInterp = linspace(xl, xr, nInterp);
+B = bbase(beamMassInterp, xl, xr, beamKnots, bdeg);
+deltabeamMassInterp = beamMassInterp(2)-beamMassInterp(1);
+
+% discrete convolution setup with design matrix G
+
+nMagnetMasses = length(magnetMasses);
+G = zeros(nMagnetMasses, nInterp);
+for iMass = 1:nMagnetMasses % a row for each manget mass
+
+    % massesInCollector are *model* masses
+    massesInCollector = collectorLimits(iMass,1) <= beamMassInterp & ...
+                        beamMassInterp <= collectorLimits(iMass,2);
+    
+    firstMassIndexInside = find(massesInCollector,1,'first');
+    lastMassIndexInside  = find(massesInCollector,1,'last');
+    G(iMass, firstMassIndexInside + 1: lastMassIndexInside -1) = deltabeamMassInterp;
+    G(iMass, [firstMassIndexInside, lastMassIndexInside]) = deltabeamMassInterp/2;
+
+end
+
+% solution for real data
+
+% trim data
+hasModelBeam = any(G,2); % magnet masses with beam model mass in collector
+G = G(hasModelBeam,:);
+magnetMasses = magnetMasses(hasModelBeam);
+measPeakIntensity = measPeakIntensity(hasModelBeam);
+
 GB = G*B;
-truePeakIntensity = G*B*testBeam;
+Wdata = diag(1./max(measPeakIntensity,1));
+%Wdata = eye(length(measPeakIntensity));
+beam4 = (GB'*Wdata*GB)\(GB'*Wdata*measPeakIntensity);
+beam6 = lsqnonneg(chol(Wdata)*GB,chol(Wdata)*measPeakIntensity);
+
 
 figure
+subplot(1,2,1)
+plot(beamMassInterp, B*beam4, '-k', 'LineWidth', 2)
+subplot(1,2,2); hold on
+plot(magnetMasses, measPeakIntensity, '-b', 'LineWidth', 2)
+plot(magnetMasses, GB*beam4, ':r', 'LineWidth', 2)
 
-subplot(2,2,1)
-plot(magnetMasses, truePeakIntensity, 'LineWidth', 2)
-subplot(2,2,2)
-hold on
-plot(beamMassInterp, B*testBeam, 'LineWidth', 2)
+% try some regularization
+%%
 
-beam1 = (G*B)\truePeakIntensity;
-plot(beamMassInterp, B*beam1, ':r', 'LineWidth', 2)
+lambda = 1e-8;
+D = diff(eye(beamKnots+bdeg), pord); % 2nd order smoothing, cubic spline;
 
-
-% ok!  add noise.
-
-measPeakIntensity = poissrnd(truePeakIntensity*0.2);
-Wdata = diag(1./max(measPeakIntensity,1));
-beam2 = (GB'*Wdata*GB)\(GB'*Wdata*measPeakIntensity);
-beam3 = GB\measPeakIntensity;
-subplot(2,2,3)
-plot(magnetMasses, measPeakIntensity, 'LineWidth', 2);
-subplot(2,2,4)
-hold on
-plot(beamMassInterp, B*beam3*5, '-b', 'LineWidth', 2)
-plot(beamMassInterp, B*beam2*5, ':r', 'LineWidth', 2)
-
-% results not the best, but not horrible!
-
-% now let's try with some smoothing
-
-lambda = 1e-10;
-D = diff(eye(beamKnots+3), 2); % 2nd order smoothing, cubic spline;
-
+%Wdata = eye(length(measPeakIntensity));
 Gaugmented = [GB; sqrt(lambda)*D];
-measAugmented = [measPeakIntensity; zeros(24,1)];
-wtsAugmented = blkdiag(Wdata, eye(24));
-beamSmooth = (Gaugmented'*wtsAugmented*Gaugmented)\(Gaugmented'*wtsAugmented*measAugmented);
-plot(beamMassInterp, B*beamSmooth*5, '-g', 'LineWidth', 2)
+measAugmented = [measPeakIntensity; zeros(beamKnots+bdeg-pord,1)];
+wtsAugmented = blkdiag(Wdata, eye(beamKnots+bdeg-pord));
+beam5 = (Gaugmented'*wtsAugmented*Gaugmented)\(Gaugmented'*wtsAugmented*measAugmented);
 
-% smoother!  need to replace magic numbers (24) above.
+figure
+subplot(1,3,1); hold on
+plot(beam5)
+plot(beam5, '.', 'MarkerSize', 20)
+subplot(1,3,2)
+plot(beamMassInterp, B*beam5)
+subplot(1,3,3); hold on
+plot(magnetMasses, measPeakIntensity, '-k')
+plot(magnetMasses, GB*beam5, '-b', 'LineWidth', 2)
+
+
+%% try to manually improve residuals
+
+GB = G*B;
+Wdata = diag(1./max(measPeakIntensity,1));
+%Wdata = eye(length(measPeakIntensity));
+beam4 = (GB'*Wdata*GB)\(GB'*Wdata*measPeakIntensity);
+% figure
+% subplot(1,2,1)
+% plot(beamMassInterp, B*beam4, '-k', 'LineWidth', 2)
+% subplot(1,2,2); hold on
+% plot(magnetMasses, measPeakIntensity, '-b', 'LineWidth', 2)
+% plot(magnetMasses, GB*beam4, ':r', 'LineWidth', 2)
+
+r = GB*beam4 - measPeakIntensity;
+r2 = (GB*beam4 - measPeakIntensity).^2;
+chi2 = r2 .* diag(Wdata);
+
