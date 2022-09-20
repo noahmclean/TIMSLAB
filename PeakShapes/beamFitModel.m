6aa23d4ce9fdbee16eb585f4443a02ce517d9592
Noah McLean <noahmc@ku.edu>
2022-09-20 16:53:31 -0500

    Start beamFitModel class
    Use for the various fitting algorithms:
    - weighted least squares
    - nonnegative least squares (also weighted)
    - penalized spline (with/without nonnegative option).
    
    Goal is to have functions to use for determining best fit collector width (varies among collectors)

--- PeakShapes/beamFitModel.m
+++ PeakShapes/beamFitModel.m
@@ -3,8 +3,14 @@ classdef beamFitModel
     %   Detailed explanation goes here
     
     properties
-        G (:,:) double
+        G (:,:) double           % integration matrix, does convolution
+        beamshape {mustBeVector} % beam shape interpolated at masses beamMassInterp
+        leftBoundary (1,1)       % low-mass boundary of beam at thesholdIntensity
+        rightBoundary (1,1)      % high-mass boundary of beam at thresholdIntensity
+
+
         
+
     end
     
     methods
