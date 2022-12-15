7418befe98939dbd582be6578b449a56b045b4fd
Noah McLean <noahmc@ku.edu>
2022-12-15 09:08:44 -0600

    add synthetic mass scan to 3seqSr

--- PeakAlignment/optimize3seqSr.m
+++ PeakAlignment/optimize3seqSr.m
@@ -35,7 +35,8 @@ Reff = 540; % mm, effective radius of magnet
 WT = (WC - WB)*[mass.Sr86 mass.Sr87 mass.Sr88]/540;
 
 isotopeMasses = [mass.Sr86 mass.Sr87 mass.Sr88];
-collectorWidths = [0.87 0.87 0.87 0.87 0.87];
+collectorWidths = [0.97 0.97 0.97 0.97 0.97];
+nCollectors = length(collectorWidths);
 
 %% reproduce a mass scan using this information
 
@@ -43,20 +44,66 @@ collectorWidths = [0.87 0.87 0.87 0.87 0.87];
 % need: beamDistInterp: x-axis for beam profile, in mm distances
 % need: beamShape: fit by beamShape routine
 
-nMassesToScan = 700;
-massScan = linspace(85.7, 86.3, nMassesToScan);
+collectorPositions = [-x(4)-x(5) -x(4) 0 x(6) x(6) + x(7)];
+collectorEdges = [collectorPositions - 0.5*collectorWidths;
+                  collectorPositions + 0.5*collectorWidths];
+nCollectorInterpPoints = 3000;
+collectorVectors = zeros(nCollectorInterpPoints, nCollectors);
+for collectorIdx = 1:5
+    collectorVectors(:,collectorIdx) = linspace(collectorEdges(1, collectorIdx), ...
+              collectorEdges(2, collectorIdx), nCollectorInterpPoints);
+end % for collectorIdx
 
-collectorPositions = [x(4)+x(5) x(4) 0 x(6) x(7)];
-collectorOpenings = [collectorPositions - 0.5*collectorWidths;
-                     collectorPositions + 0.5*collectorWidths];
+
+nScans = 3;
+nMassesToScan = 700;
+
+scanLimits = [85.7, 86.12; 86.7, 87.12; 87.7, 88.12];
+
+for iScan = 1:nScans
+
+massScanMass = linspace(scanLimits(iScan, 1), scanLimits(iScan,2), nMassesToScan);
+% nMassesToScan = 1;
+% massScan = 85.91;
+
+massScanIntensity = zeros(nMassesToScan, nCollectors);
+
 
 
 for iMass = 1:nMassesToScan
 
-    axialMass = massScan(iMass);
+    axialMass = massScanMass(iMass);
     dispersion = Reff/axialMass;
     isotopePositions = (isotopeMasses - axialMass)*dispersion;
 
+    for collectorIndex = 1:5 % [L3 L2 Ax H1 H2] = [1 2 3 4 5]
+
+        thisCollectorVector = collectorVectors(:, collectorIndex);
+
+        for isotopeIndex = 1:3 % [86Sr 87Sr 88Sr] = [1 2 3]
+
+            beamShapePosition = beamDistInterp + isotopePositions(isotopeIndex);
+
+            beamOverlap = interp1(beamShapePosition, beamShapeNorm, thisCollectorVector, 'pchip', 0);
+
+            massScanIntensity(iMass, collectorIndex) = ...
+                massScanIntensity(iMass, collectorIndex) + ...
+                trapz(thisCollectorVector, beamOverlap);
+
+        end % for isotopeIndex
+
+
+    end % for collectorIndex
 
 
 end % for iMass
+
+subplot(nScans,1,iScan)
+plot(massScanMass, massScanIntensity, 'LineWidth', 2)
+line([x(iScan) x(iScan)], [0 1.05], 'Color', 'k', 'LineWidth', 1, 'LineStyle', '--')
+legend({'L3', 'L2', 'Ax', 'H1', 'H2'})
+set(gca, 'FontSize', 16)
+ylim([0 1.05])
+end % for iScan
+
+
