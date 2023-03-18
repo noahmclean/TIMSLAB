8e84502805c4589aedddeb1a3bc1f67a4de4f1fa
Noah McLean <noahmc@ku.edu>
2023-03-18 16:59:15 -0500

    start parsing timing for synthetic data

--- SyntheticData/src/makeSyntheticData.m
+++ SyntheticData/src/makeSyntheticData.m
@@ -4,7 +4,8 @@
 % add an ion beam intensity time-series
 % output: .TXT file in syndata subfolder
 % 
-% created by Noah McLean for Tripoli and FaradayRelativeEfficiencies on 28-Feb-2023
+% created by Noah McLean for Tripoli and FaradayRelativeEfficiencies 
+% on 28-Feb-2023
 
 %% 1. Setup 
 
@@ -16,7 +17,8 @@ refmat = referenceMaterial("NBS981");
 methodName = "Pb 4-5-6-7-8 Daly 10-5-5-5-2 sec.TIMSAM";
 nBlocks = 10;
 intensityFunction = @(t) 1e6; % cps of major isotope
-%intensityFunction = @(ampl, freq, minInt, t)  ampl*(ceil(freq*t)-freq*t)+minInt; % sawtooth
+%intensityFunction = @(ampl, freq, minInt, t)  ...
+%      ampl*(ceil(freq*t)-freq*t)+minInt; % sawtooth
 
 % isotopic fractionation for Faradays and Ion Counters
 betaFaraday = -0.2; % 0.10%/amu at Pb mass
@@ -31,4 +33,40 @@ method = parseTIMSAM(methodName);
 CollNames = ["L5", "L4", "L3", "L2", "Ax", "PM", "H1", "H2", "H3", "H4"];
 method = processMethod(method, CollNames);
 
-%% 2.  
+%% 2. Piece out timing from method
+
+nBaselines = size(method.baselines,2);
+nOnPeaks   = size(method.onpeaks,2);
+
+% extract integration period and time for each baseline
+integrations.BL.n = zeros(nBaselines,1);
+integrations.BL.integPeriods = zeros(nBaselines,1);
+for iBL = 1:nBaselines
+    
+    integTime   = method.baselines(iBL).Info(6).Value; % integration time
+    integTime   = str2double(integTime);               % integration time, s
+    integPeriod = method.baselines(iBL).Info(5).Value; % integ period (string)
+    integPeriod = str2double(integPeriod(3:end));      % integ period, ms
+    integrations.BL.n(iBL) = integTime/(integPeriod/1e3);
+    integrations.BL.integPeriods(iBL) = integPeriod/1e3; % in seconds
+
+    integrations.OP
+
+end % for iBL
+
+% extract integration period and time for each OP sequence
+integrations.OP.n = zeros(nOnPeaks,1);
+integrations.OP.integPeriods = zeros(nOnPeaks,1);
+for iOP = 1:nOnPeaks
+
+    integTime   = method.onpeaks(iOP).Info(7).Value; % integration time, s
+    integTime   = str2double(integTime);               % integration time, s
+    integPeriod = method.onpeaks(iOP).Info(6).Value; % integ period (string)
+    integPeriod = str2double(integPeriod(3:end));  % integ period, ms
+    integrations.OP.n(iOP) = integTime/(integPeriod/1e3);
+    integrations.OP.integPeriods(iOP) = integPeriod/1e3; % in seconds
+
+end
+
+nCyclesPerBlock = str2double(method.settings(5).Value);
+%integrations.OP.settleTime = 
